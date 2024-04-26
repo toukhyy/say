5fd6f4d3559c478aa3e676f46e1c822c4f489377
toukhyy <tokhy104@gmail.com>
2024-04-26 18:32:47 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Container, Text } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <Container maxW="container.sm">
+      <Text fontSize="3xl" bg="blue">
+        say
+      </Text>
+    </Container>
+  );
+}
+
+export default App;
